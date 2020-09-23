e124fee283ec9da690ba8051820cc9a8b857f55d
Volodymyr Pestov <pestov.v@yahoo.com>
2020-09-23 15:49:09 +0300

    Updates

---
+++ precache-manifest.37d341eba4a3f5dd0fc737920239ac79.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "449c72e803b6dcb32ecef2c59b750bff",
+    "url": "/social/index.html"
+  },
+  {
+    "revision": "55ca8b2aa93446056a0f",
+    "url": "/social/static/css/main.93e1d843.chunk.css"
+  },
+  {
+    "revision": "2bfbba7a2de476d458d6",
+    "url": "/social/static/js/2.eee1b8d8.chunk.js"
+  },
+  {
+    "revision": "b4bb1fb0e0439930a57fb2a627c1300e",
+    "url": "/social/static/js/2.eee1b8d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "55ca8b2aa93446056a0f",
+    "url": "/social/static/js/main.e28a6af2.chunk.js"
+  },
+  {
+    "revision": "245b1d1fb60e0de385d4",
+    "url": "/social/static/js/runtime-main.3c0aa0bb.js"
+  },
+  {
+    "revision": "592bb707e9a233fc30f60f2b2618223d",
+    "url": "/social/static/media/main.592bb707.jpg"
+  },
+  {
+    "revision": "fc3ed7c960c2099ecaa17c9fb25e83d8",
+    "url": "/social/static/media/man.fc3ed7c9.svg"
+  }
+]);
